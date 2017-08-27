804dd41ca4dd7937c3804fcda050e0dfbaa21385
Alexandre Jacquin <kinou@rilynett.com>
2017-08-27 20:30:22 +0200

    add missing Alsbeth the Discordant

--- main.lua
+++ main.lua
@@ -21,7 +21,6 @@ KL.rares = {
     ["U1AD8C16047F09E16"] = {name = "C1A3 Recon Model IIV Mk3.0"},
     ["U1AD8C15D15766D22"] = {name = "C2A1 Battle Model II Mk1"},
     ["U1AD8C16261070474"] = {name = "Tornadax"},
-    ["U1AD8C15A6AF53CFF"] = {name = "Orinash Windborne"},
     ["U1AD8C1685E02E58F"] = {name = "Primus Eria"},
     ["U0ED05A1E11C334F0"] = {name = "Trenton Zozula"},
     ["U0ED05A203317A2D6"] = {name = "Ai-Orii-i-iah"},
@@ -37,6 +36,7 @@ KL.rares = {
     ["U1AD8C1551E2405B5"] = {name = "Ai-ichak"},
     ["U0ED05A226CBA2835"] = {name = "Walking Death"},
     ["U1AD8C1562F12C8BA"] = {name = "Experiment Alpha-2"},
+    ["U522B0D87291475C2"] = {name = "Alsbeth the Discordant"},
 }
 
 

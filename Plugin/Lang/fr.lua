f973b1ee4f1563ba4b1dc8e077a563f63cbf9a7a
Maxxi <maxxi@live.fr>
2017-09-04 20:11:21 +0200

    Add Lang Add button to open

---
+++ Plugin/Lang/fr.lua
@@ -0,0 +1,20 @@
+	--[[
+		Ranadyla@Brisesol
+		Guilde No Play Time
+	]]--
+	
+	-- NE MODIFIE PAS CE FICHIER DIRECTEMENT. Créé en un nouveau pour votre langue
+	Library.Translate.Fr(
+	{
+		JUSTKILLED = "vous venez de tuer le rare: ",
+		LASTRESET = "dernière réinitialisation: ",
+		STILLKILL = "Vous devez encore tuer: \n",
+		ALLRAREKILLED = "Tous les rares ont été tués aujourd'hui !!!",
+		DEBUGON = "debug activé",
+		DEBUGOFF = "debug désactivé",
+		YES = "Oui",
+		NO = "Non",
+		NEEDUIRELOAD = "Vous devez recharger votre interface",
+		KLBUTTONACTIVE = "Bouton actif",
+		KLBUTTONNOTACTIVE = "Bouton inactif",
+	})
---
+++ Plugin/buttonDrag.lua
@@ -0,0 +1,114 @@
+local toc, KL = ...
+local AddonId = toc.identifier
+local Lang = Library.Translate
+
+function KL.buttonMover(buttonName, parentFrame, visibleFrame, imgRootUp, imgNameUp, imgRootDown, imgNameDown, KL_mouseDataX, KL_mouseDataY, KL_buttonActive)
+    local   buttonName = UI.CreateFrame("Texture", buttonName, parentFrame)
+            buttonName:SetPoint("TOPLEFT", parentFrame, "TOPLEFT", KL_mouseDataX, KL_mouseDataY)
+            if imgNameDown then
+                buttonName:SetTexture(imgRootDown, imgNameDown)
+            end
+            if KL_buttonActive == true then
+                buttonName:SetVisible(true)
+            else                
+                buttonName:SetVisible(false)
+            end
+
+            buttonName:EventAttach(Event.UI.Input.Mouse.Left.Click, function(self)
+                if imgNameUp then
+                    self:SetTexture(imgRootUp, imgNameUp)
+                end
+                if not visibleFrame.visible then
+                    visibleFrame:SetVisible(true)
+                else
+                    visibleFrame:SetVisible(false)
+                end 
+                visibleFrame.visible = not visibleFrame.visible
+            end, "dragLeftClick")
+
+            buttonName:EventAttach(Event.UI.Input.Mouse.Cursor.In, function(self)
+                if imgNameUp then
+                    self:SetTexture(imgRootUp, imgNameUp)
+                end
+            end, "dragCursorIn")
+
+            buttonName:EventAttach(Event.UI.Input.Mouse.Cursor.Out, function(self)
+                if not visibleFrame.visible then
+                    if imgNameDown then
+                        self:SetTexture(imgRootDown, imgNameDown)
+                    end
+                end
+            end, "dragCursorOut") 
+
+            KL.buttonMovable(buttonName, parentFrame)
+
+    return buttonName
+end
+
+function KL.buttonMovable(buttonName, parentFrame)
+    if parentFrame == nil then
+        parentFrame    = buttonName
+    end
+
+    function buttonName.Event:RightDown()
+        InspectMouse    = Inspect.Mouse()
+        self.MouseDown  = true
+        self.MyStartX   = parentFrame:GetLeft()
+        self.MyStartY   = parentFrame:GetTop()
+        self.StartX     = KL_mouseData.x - self.MyStartX
+        self.StartY     = KL_mouseData.y - self.MyStartY
+    end
+
+    function buttonName.Event:MouseMove(mouseX, mouseY)
+        if self.MouseDown then
+            parentFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", (mouseX - self.StartX), (mouseY - self.StartY))
+            -- Nouvelles coordonnées à enregistrer --
+            KL_mouseData.x = mouseX
+            KL_mouseData.y = mouseY
+        end
+    end
+
+    function buttonName.Event:RightUp()
+        self.MouseDown  = false
+    end
+end
+
+function KL.PopUp(DialogContext, DialogText, DialogSize, DialogCmd)
+    local   PopUpContext = UI.CreateContext(DialogContext)
+            PopUpContext:SetPoint("CENTER", UIParent, "CENTERTOP", 0, 100)
+            PopUpContext:SetStrata("topmost")
+
+            -- Création de la barre permanente --
+            PopUpdialog = UI.CreateFrame("Texture", AddonId.."_PopUpdialog", PopUpContext)
+            PopUpdialog:SetVisible(true)
+            PopUpdialog:SetWidth(350)
+            PopUpdialog:SetHeight(100)
+            PopUpdialog:SetTexture("Rift", "ItemToolTip_I75.dds")
+            PopUpdialog:SetPoint("CENTER", UIParent, "CENTERTOP", 0, 100)
+
+            PopUpText = UI.CreateFrame("Text", AddonId.."_ChannelText", PopUpdialog)
+            PopUpText:SetText(DialogText)
+            PopUpText:SetFontSize(DialogSize)
+            PopUpText:SetPoint("TOPCENTER", PopUpdialog, "TOPCENTER", 0, 15)
+
+            PopUpButtonYes = UI.CreateFrame("RiftButton", AddonId.."_PopUpButtonYes", PopUpdialog)
+            PopUpButtonYes:SetText(Lang.YES)
+            PopUpButtonYes:SetPoint("BOTTOMLEFT", PopUpdialog, "BOTTOMLEFT", 20, -10)
+
+            PopUpButtonNo = UI.CreateFrame("RiftButton", AddonId.."_PopUpButtonNo", PopUpdialog)
+            PopUpButtonNo:SetText(Lang.NO)
+            PopUpButtonNo:SetPoint("BOTTOMRIGHT", PopUpdialog, "BOTTOMRIGHT", -20, -10)
+
+            PopUpContext:SetSecureMode("restricted")
+            PopUpdialog:SetSecureMode("restricted")
+            PopUpButtonYes:SetSecureMode("restricted")
+
+            PopUpButtonYes:EventAttach(Event.UI.Button.Left.Press, function(self)
+                PopUpContext:SetVisible(false)
+                self:EventMacroSet(Event.UI.Input.Mouse.Left.Click, DialogCmd)
+            end, "Event.UI.Button.Left.Press")
+
+            PopUpButtonNo:EventAttach(Event.UI.Button.Left.Press, function()
+                PopUpContext:SetVisible(false)
+            end, "Event.UI.Button.Left.Press")
+end
